0b157bbe8f2c54ec7358f2ae16d3974034c28174
Xiodanny-Vasquez <xiodanny.vasquez@soy.sena.edu.co>
2024-10-28 22:27:56 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Boton from './Boton';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Prueba de Componente Botón</h1>
+      <Boton />
+    </div>
+  );
+}
+
+export default App;
